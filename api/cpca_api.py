856b66a9b9ce980fda678aebd87b6971a00ba27c
Wistral <whistral@gmail.com>
2019-10-13 00:24:37 +0800

    根据公司名返回省市名

---
+++ api/cpca_api.py
@@ -0,0 +1,16 @@
+import cpca
+
+
+def corps_to_addr(corps: [str]) -> [(str, str)]:
+    '''传入公司名列表, 返回元组列表, 元组包含省市信息
+    
+    >>> location_str = ['杭州佩灵轴承有限公司', '乐清市乐清港湾区投资发展有限公司']
+    >>> corps_to_addr(location_str)
+    [('浙江省', '杭州市'), ('浙江省', '温州市')]
+    '''
+    df = cpca.transform(corps)
+    print(df)
+    prov = df['省'].tolist()
+    city = df['市'].tolist()
+    return [(e, city[i]) for i, e in enumerate(prov)]
+    
