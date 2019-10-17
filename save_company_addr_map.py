821c3d13e1c3d813162b6112dfc4fbf70ffaada6
hjwblog.com <yysz169hjw@126.com>
2019-10-17 15:56:07 +0800

    爬取公司地址脚本

---
+++ save_company_addr_map.py
@@ -0,0 +1,20 @@
+import os
+
+from dao import companyAddr
+from excel.readExistInfo import get_corp_addr_map_and_names
+
+
+
+
+
+if __name__ == '__main__':
+    corp_addr_map = {}
+
+    for root, dirs, files in os.walk(os.path.join('.', '2009-2015联合申请专利')):
+        for file in files:
+            if file[-3:] == 'xls':
+                name_map, _ = get_corp_addr_map_and_names(os.path.join(root, file))
+                corp_addr_map.update(name_map)
+
+
+    companyAddr.saveDic(corp_addr_map)
